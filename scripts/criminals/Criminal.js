a799ab0170de5bdf666c75bc38421758a2d650f8
bshoop <braxton.shoop@gmail.com>
2020-08-31 15:52:03 -0500

    Finished formatting. Book 2 Chapter 2 is now finished.

--- scripts/criminals/Criminal.js
+++ scripts/criminals/Criminal.js
@@ -4,11 +4,14 @@ console.log("Criminal.js");
 export const CriminalsHTML = (criminalObj) => {
     return `
         <section id="criminal-${criminalObj.id}" class="card-criminal">
-            <h2>${criminalObj.name}</h2>
-            <p>Age: ${criminalObj.age}<p>
-            <p>Crime: ${criminalObj.conviction}</p>
-            <p>Start: ${new Date(criminalObj.incarceration.start).toLocaleDateString('en-US')}</p>
-            <p>End: ${new Date(criminalObj.incarceration.end).toLocaleDateString('en-US')}</p>
+            <ul>
+                <li><h2>${criminalObj.name}</h2></li>
+                <br>
+                <li><strong><p>Age: ${criminalObj.age}<p></strong></li>
+                <li><strong><p>Crime: ${criminalObj.conviction}</p></strong></li>
+                <li><strong><p>Term start: ${new Date(criminalObj.incarceration.start).toLocaleDateString('en-US')}</p></strong></li>
+                <li><strong><p>Term end: ${new Date(criminalObj.incarceration.end).toLocaleDateString('en-US')}</p></strong></li>
+            </ul>
         </section>
     `
 };
